2def57b1a87280674dcd057d19b1c7ddc9850b65
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-09-18 12:18:56 +0100

    Machine class can return its own jackpot

---
+++ lib/machine.rb
@@ -0,0 +1,7 @@
+class Machine
+  attr_reader :jackpot
+
+  def initialize(jackpot)
+    @jackpot = jackpot
+  end
+end
--- spec/machine_spec.rb
+++ spec/machine_spec.rb
@@ -1,1 +1,11 @@
 require 'machine'
+
+describe Machine do
+  let(:subject) { Machine.new(500) }
+
+  describe '#jackpot', :jackpot do
+    it 'Returns the current machine jackpot' do
+      expect(subject.jackpot).to eq(500)
+    end
+  end
+end
