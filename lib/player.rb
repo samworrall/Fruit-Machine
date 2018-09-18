054c2ac673dc4ef8236b37828de5ac3fda032be7
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-09-18 11:46:39 +0100

    Add wallet attr_reader to Player

---
+++ lib/player.rb
@@ -0,0 +1,7 @@
+class Player
+  attr_reader :wallet
+
+  def initialize
+    @wallet = 50
+  end
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,9 @@
 require 'player'
+
+describe Player do
+  describe '#wallet' do
+    it 'Returns the value of the players wallet' do
+      expect(subject.wallet).to eq(50)
+    end
+  end
+end
