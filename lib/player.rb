e732c17ff57e00f35d28abbac05aab3b83d16ecd
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-09-18 12:01:31 +0100

    Player initialises with a name

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,8 @@
 class Player
-  attr_reader :wallet
+  attr_reader :name, :wallet
 
-  def initialize(wallet_size)
+  def initialize(name, wallet_size)
+    @name = name
     @wallet = wallet_size
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,15 +1,21 @@
 require 'player'
 
 describe Player do
-  let(:subject) { Player.new(50) }
+  let(:subject) { Player.new('Sam', 50) }
 
   context 'Different wallet value on initialisation' do
-    let(:subject) { Player.new(100) }
+    let(:subject) { Player.new('Sam', 100) }
 
     describe '#initialize', :init do
       it 'Sets the players balance based on argument given on initialisation' do
         expect(subject.wallet).to eq(100)
       end
+    end
+  end
+
+  describe '#name', :name do
+    it 'Returns the player name' do
+      expect(subject.name).to eq('Sam')
     end
   end
 
