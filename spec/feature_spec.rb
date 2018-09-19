5be0bc8f3c69478ee56db1671fc46bc85d385ff6
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-09-19 10:55:22 +0100

    Wallet decrease by 1, jackpot increase by 1 when all slot colours are unique

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -3,5 +3,23 @@ require 'machine'
 require 'player'
 
 describe 'Feature', :feature do
-  
+  let(:player) { Player.new('Sam', 50) }
+  let(:machine) { Machine.new(100) }
+  let(:game) { Game.new(machine, player) }
+
+  context 'All slot colours are different' do
+    before(:each) do
+      srand(3)
+    end
+
+    describe 'All slot colours are different' do
+      it 'Reduces the player wallet by 1' do
+        expect{ game.play }.to change{ player.wallet }.by(-1)
+      end
+
+      it 'Increases the machine jackpot by 1' do
+        expect{ game.play }.to change{ machine.jackpot }.by(1)
+      end
+    end
+  end
 end
