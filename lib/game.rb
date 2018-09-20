abdd0715557c53b3f43d614c6e481523b6a080c6
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-09-20 09:31:35 +0100

    Update game logic

--- lib/game.rb
+++ lib/game.rb
@@ -9,11 +9,11 @@ class Game
   def play
     @machine.spin
     if @machine.all_slots_equal
-      @player.update_wallet(@machine.jackpot)
-      @machine.update_jackpot(-(@machine.jackpot))
+      @player.update_wallet(jackpot)
+      @machine.update_jackpot(-(jackpot))
     elsif @machine.all_slots_unique
-      @player.update_wallet(@machine.jackpot / 2)
-      @machine.update_jackpot(-(@machine.jackpot / 2))
+      @player.update_wallet(half_jackpot)
+      @machine.update_jackpot(-(half_jackpot))
     elsif @machine.two_or_more_adjacent_slots_equal
       @player.update_wallet(5)
       @machine.update_jackpot(-5)
@@ -21,5 +21,13 @@ class Game
       @player.update_wallet(-1)
       @machine.update_jackpot(1)
     end
+  end
+
+  def jackpot
+    @machine.jackpot
+  end
+
+  def half_jackpot
+    @machine.jackpot / 2
   end
 end
